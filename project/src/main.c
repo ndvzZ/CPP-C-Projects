ce06682122a468c17c12848207f8eb59c2da0f09
DenisN <nagaytsev_den@mail.ru>
2022-03-19 17:26:17 +0300

    second-first-commit

--- project/src/main.c
+++ project/src/main.c
@@ -1,4 +1,4 @@
-struct masterRecord{ int 		Number; char 		Name[20]; char 		Surname[20]; char 		addres[30]; char 		TelNumber[15]; double  	indebtedness; double    	credit_limit;  double  	cash_payments; };
+struct masterRecord{ int Number; char Name[20]; char Surname[20]; char addres[30]; char TelNumber[15]; double indebtedness; double credit_limit;  double cash_payments;};
 	typedef  struct  masterRecord Data;
 	int main(void){
 		int choice = 0;	
