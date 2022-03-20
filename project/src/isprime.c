8bdaa86457de1641611dc0952cd75ec8ec504b5c
DenisN <nagaytsev_den@mail.ru>
2022-03-20 09:37:55 +0300

    fixed_isPr

--- project/src/isprime.c
+++ project/src/isprime.c
@@ -3,8 +3,6 @@
 int is_prime(int num) {
     if (num < 2) {
         return 0;
-    } else if (num < 0) {
-    return 0;
     } else {
         for (int i = 2; i*i < num; i++) {
             if (num % i == 0) return 0;
